d6c07961c56c4cb4d70a4f169c81cd141d14a6d4
Redo76 <redouane.hamadache1@gmail.com>
2022-09-15 09:14:07 +0200

    Changement header

--- app.js
+++
@@ -1,9 +0,0 @@
-function menuOnClick() {
-    document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-}
-
-var myCarousel = document.querySelector('#carouselBasicExample');
-var carousel = new bootstrap.Carousel(myCarousel);
-
