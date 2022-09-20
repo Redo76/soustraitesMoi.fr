a79bcff84a5fc4a0c49686d384292e5f9f674905
Redo76 <redouane.hamadache1@gmail.com>
2022-09-20 17:51:45 +0200

    Modifs css page d'accueil

--- src/Controller/ExpertController.php
+++ src/Controller/ExpertController.php
@@ -9,7 +9,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class ExpertController extends AbstractController
 {
     #[Route('/mon-compte-expert', name: 'app_expert')]
-    #[IsGranted('ROLE_EXPERT')]
+    // #[IsGranted('ROLE_EXPERT')]
+    // #[IsGranted('ROLE_ADMIN')]
     public function index(): Response
     {
         return $this->render('profil_expert/index.html.twig', [
